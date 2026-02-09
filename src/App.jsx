9acf2b235e5a7408148669ea9c57922e46bf7ba1
esthertozzo <tozzo.est@gmail.com>
2026-02-09 18:34:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <main>
+    <header>
+      <img src="/logo.png" alt="Logo" />
+    </header>
+    <section>
+      <img src="/banner.png" alt="Banner" />
+    </section>
+   </main>
+  )
+}
+
+export default App
